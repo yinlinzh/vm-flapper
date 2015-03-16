4277b7691064fa90eda0ba5ab026a610b87ce7ff
Yinlin Zhang <yinlinzh@gmail.com>
2015-03-16 20:08:25 +0800

    explore flask error handler & static files & Jinja2 template

--- OReillyFlask/hello.py
+++ OReillyFlask/hello.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, make_response, abort, redirect, render_template
+from datetime import datetime
+
+from flask import Flask, request, make_response, abort, redirect, render_template, url_for
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 
 # Create an application instance, an object of class Flask
 # The only required argument to the Flask class constructor
 # is the name of the main module or package of the application.
 app = Flask(__name__)
 mgr = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 # view function
@@ -15,7 +21,7 @@ mgr = Manager(app)
 # with HTML content
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
     # user_agent = request.headers.get('User-Agent')
     # response = make_response('<h1>Your User-Agent is %s\n, This document carries a cookie!</h1>' % user_agent)
     # response.set_cookie('answer', '42')
@@ -27,9 +33,20 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name='<h1>One more time</h1>') #name)
+    url = url_for('user', name=name, _external=True)
+    return render_template('user.html', name=name, url=url)
     # abort(404)
     # return '<h1>Hello, %s!</h1>' % name
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
