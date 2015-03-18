83d6323aad52b1ed97d4fd1737366df18e49dba9
Yinlin Zhang <yinlinzh@gmail.com>
2015-03-18 11:40:35 +0800

    try things with flask-wtf extension and use bootstrap wtf to render web forms

--- OReillyFlask/hello.py
+++ OReillyFlask/hello.py
@@ -2,26 +2,54 @@
 
 from datetime import datetime
 
-from flask import Flask, request, make_response, abort, redirect, render_template, url_for
+from flask import Flask, request, make_response, abort, redirect, render_template, \
+                  url_for, session, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 # Create an application instance, an object of class Flask
 # The only required argument to the Flask class constructor
 # is the name of the main module or package of the application.
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 mgr = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
 # view function
 # A response returned by a view function can be a simple string
 # with HTML content
-@app.route('/')
+# The methods argument added to the app.route decorator tells Flask to register the view function
+# as a handler for GET and POST requests in the URL map.
+# When methods is not given, the view function is registered to handle GET requests only.
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            # flash
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        # The local "name" variable is now placed in the user session as session['name'] so that
+        # it is remembered beyond the request.
+        return redirect(url_for('index'))
+    return render_template('index.html',
+                           current_time=datetime.utcnow(),
+                           name=session.get('name'),
+                           form=form)
     # user_agent = request.headers.get('User-Agent')
     # response = make_response('<h1>Your User-Agent is %s\n, This document carries a cookie!</h1>' % user_agent)
     # response.set_cookie('answer', '42')
