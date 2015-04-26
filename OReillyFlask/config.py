b2ba87496a38d0cd9132550baa70fc5d81a942a4
Yinlin Zhang <yinlinzh@gmail.com>
2015-04-26 23:10:55 +0800

    add email confirmation

--- OReillyFlask/config.py
+++ OReillyFlask/config.py
@@ -7,9 +7,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SECRET_KEY = os.environ.get('SECRET_KEY', default='hard to guess string')
+    # The application configured automatic database commits at the end of the request.
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[FlaskyAsk]'
+    FLASKY_MAIL_SENDER = 'Yinlin Zhang <flaskaskmail@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
